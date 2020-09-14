badc968cec7085d882f89ff2b7d8675eed5751ea
Muhammad Iqbal Kurniawan <iqbal@lixus.id>
2020-09-14 08:55:38 +0700

    added bottom-up to fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -19,8 +19,23 @@ func fibMemoized(n int, m map[int]int) int {
 	return m[n]
 }
 
+func fibBottomUp(n int) int {
+	if n < 2 {
+		return n
+	}
+	table := make([]int, n+1)
+	table[0] = 0
+	table[1] = 1
+	table[2] = 1
+	for i := 3; i <= n; i++ {
+		table[i] = table[i-1] + table[i-2]
+	}
+	return table[n]
+}
+
 func main() {
 	// fmt.Println(fibonacci(6))
 
-	fmt.Println(fibMemoized(6, map[int]int{}))
+	// fmt.Println(fibMemoized(6, map[int]int{}))
+	fmt.Println(fibBottomUp(6))
 }
